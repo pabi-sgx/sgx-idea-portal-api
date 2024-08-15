27fc65296d013abfdc781878d2ef48aec7eff064
pabitra-at-414227014137 <mailtopabi@gmail.com>
2024-08-15 19:12:34 +0530

    added apis

---
+++ src/routes/ideasRoute.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getIdeas, saveIdea, updateIdea, getIdea } from '../controllers/ideasController';
+import {}
+
+// Create a router
+export const ideasRouter = express.Router();
+// Define routes with '/ideas' as root path
+ideasRouter.get('/ideas', getIdeas);
+ideasRouter.post('/ideas', saveIdea);
+ideasRouter.put('/ideas/:id', updateIdea);
+ideasRouter.get('/ideas/:id', getIdea);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import ideasRouter from './routes';
 
 const app = express();
 
-app.get('/welcome', (req, res) => {
-  res.json({ message: 'Welcome to the SGX Idea Portal API!' });
-});
-
+// Use the router
+app.use(ideasRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
