70a0bced45bb3b7a494109df407bd30c1af1183c
Pabitra Muni <mailtopabi@gmail.com>
2024-09-07 13:43:07 +0530

    db changes

--- src/model/Idea.ts
+++ src/model/Idea.ts
@@ -9,13 +9,18 @@ export interface IIdea extends Document {
     status: string;
 }
 
+// schema options
+var options = {
+    timestamps: true
+};
+
 const IdeaSchema: Schema = new Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true},
+    description: { type: String, required: true },
     fullName: { type: String, required: true },
-    employeeID: { type: String, required: true},
+    employeeID: { type: String, required: true },
     status: { type: String, required: true }
-});
+}, options);
 
 export default mongoose.model<IIdea>('Idea', IdeaSchema);
 
